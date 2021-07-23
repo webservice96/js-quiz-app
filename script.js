5b6d941b2bd76aeccf94400ed13948fc2c0a37aa
Helal Uddin <dmdhelal@gmail.com>
2021-07-23 23:22:30 +0600

    Quiz app design

---
+++ script.js
@@ -0,0 +1,48 @@
+const quizData = [{
+        question: 'How many things are needed to learn for a full stack developer?',
+        a: 'Html, Css',
+        b: 'Html, Css, Php, Javascript, Mysql',
+        c: 'Php, Java, MongoDB',
+        d: 'Ruby, Paython',
+        correct: 'b'
+    }, {
+        question: 'What is the most used programing language?',
+        a: 'Java',
+        b: 'C',
+        c: 'PHP',
+        d: 'Javascript',
+        correct: 'd'
+    },
+    {
+        question: 'How many days need to learn any programing language?',
+        a: '20 Days',
+        b: '25 Days',
+        c: '6 Month',
+        d: '1 Year',
+        correct: 'c'
+    },
+    {
+        question: 'Where is the Fiverr Head Office?',
+        a: 'Bangladesh',
+        b: 'USA',
+        c: 'Israel',
+        d: 'India',
+        correct: 'c'
+    },
+    {
+        question: 'When did Github Start their Service?',
+        a: 'Feb 2008',
+        b: 'Feb 1989',
+        c: 'Jun 2012',
+        d: 'Aug 2020',
+        correct: 'a'
+    }, {
+        question: 'What year was Javascript launched?',
+        a: '2008',
+        b: '2000',
+        c: '2012',
+        d: 'None of the above',
+        e: 'Jan 1999',
+        correct: 'd'
+    }
+]
